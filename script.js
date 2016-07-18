743466d32c0cb0e4dfff81ece8941dd0622a0aea
pietrzakacper <kacpietrzak@gmail.com>
2016-07-18 18:26:05 +0200

    Revert head: wrong repo :(

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-function returnQuote(json){
-  document.getElementById("quote-text").innerHTML = json.quote;
-  document.getElementById("author").innerHTML = json.author+Math.floor(Math.random() * 20);
-}
-
-
 function getRandomQuote() {
     $.ajax({
-        dataType: 'jsonp',
-        url: 'http://localhost/RandomQuoteGenerator/getQuote.php',
-        jsonpCallback: "returnQuote",
-        error: function() {
-        console.log("Coś poszło nie tak :(");
+        headers: {
+            "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+        success: function(response) {
+                var jsonObj = JSON.parse(response);
+                document.getElementById("quote-text").innerHTML = jsonObj.quote;
+                document.getElementById("author").innerHTML = jsonObj.author;
       }
     });
 }
